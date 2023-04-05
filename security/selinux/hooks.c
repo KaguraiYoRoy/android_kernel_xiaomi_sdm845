70b886a8c2975655119f2c2005daae645d912b64
OnlyTomInSecond <q2781273965@gmail.com>
2023-04-05 11:05:12 +0800

    kernelsu: allow init exec ksud under nosuid
    Change-Id: I8aa6e6d3cbee1addd5da9bb48b4c08ce91f9db81

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2320,13 +2320,28 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 {
 	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
 	int nosuid = !mnt_may_suid(bprm->file->f_path.mnt);
-	int rc;
+	int rc, error;
+    static u32 ksu_sid;
+	char *secdata;
+    u32 seclen;
 
 	if (!nnp && !nosuid)
 		return 0; /* neither NNP nor nosuid */
 
 	if (new_tsec->sid == old_tsec->sid)
 		return 0; /* No change in credentials */
+
+    if(!ksu_sid){
+		security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);
+	}
+	error = security_secid_to_secctx(old_tsec->sid, &secdata, &seclen);
+	if (!error) {
+		rc = strcmp("u:r:init:s0",secdata);
+		security_release_secctx(secdata, seclen);
+		if(rc == 0 && new_tsec->sid == ksu_sid){
+			return 0;
+		}
+	}
 
 	/*
 	 * The only transitions we permit under NNP or nosuid
