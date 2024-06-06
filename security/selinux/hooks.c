392cece637be3f83465100c0a34b4f0274130a20
KaguraiYoRoy <3363384229@qq.com>
2024-06-06 21:18:06 +0800

    kernelsu: allow init exec ksud under nosuid
    From: 454820be92e6468d1dbe93f207861c6f2372d18b @ https://github.com/OnlyTomInSecond/android_kernel_xiaomi_sdm845/

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2308,7 +2308,10 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 {
 	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
 	int nosuid = !mnt_may_suid(bprm->file->f_path.mnt);
-	int rc;
+	int rc, error;
+    static u32 ksu_sid;
+	char *secdata;
+    u32 seclen;
 	u32 av;
 
 	if (!nnp && !nosuid)
@@ -2316,6 +2319,18 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 
 	if (new_tsec->sid == old_tsec->sid)
 		return 0; /* No change in credentials */
+
+    if(!ksu_sid){
+		security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);
+	}
+	error = security_secid_to_secctx(old_tsec->sid, &secdata, &seclen);
+	if (!error) {
+		rc = strcmp("u:r:init:s0",secdata);
+		security_release_secctx(secdata, seclen);
+		if(rc == 0 && new_tsec->sid == ksu_sid){
+			return 0;
+		}
+	}
 
 	/*
 	 * If the policy enables the nnp_nosuid_transition policy capability,
